d7706ae6f04aa22f5665f6f88da1fb488d308b7f
Criiss22g <criiss22g@gmail.com>
2024-11-18 15:29:02 +0100

    segundo commit

--- src/App.java
+++ src/App.java
@@ -11,11 +11,11 @@ public class App {
             }
         }
         //CABALLOS NEGROS
-        tablero[7][1] = " cn ";
-        tablero[7][6] = " cn ";
+        tablero[7][1] = " cb ";
+        tablero[7][6] = " cb ";
         //CABALLOS BLANCOS
-        tablero[0][1] = " cb ";
-        tablero[0][6] = " cb ";
+        tablero[0][1] = " cn ";
+        tablero[0][6] = " cn ";
 
         for (int i = 0; i < tablero.length; i++) {
             System.out.println(Arrays.toString(tablero[i])); 
